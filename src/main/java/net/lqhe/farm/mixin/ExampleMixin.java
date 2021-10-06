929a03189fa1d801189326f2f65ff453503a839c
liquidhelium <liquidhelium@163.com>
2021-10-06 19:56:04 +0800

    optimize and change the trigger to attacking the crop

--- src/main/java/net/lqhe/farm/mixin/ExampleMixin.java
+++ src/main/java/net/lqhe/farm/mixin/ExampleMixin.java
@@ -1,24 +1,38 @@
 package net.lqhe.farm.mixin;
 
+import net.lqhe.farm.util.FarmUtil;
+import net.minecraft.block.CropBlock;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
-import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import org.lwjgl.system.CallbackI;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import net.lqhe.farm.util.onMessage;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayNetworkHandler.class)
+@Mixin(MinecraftClient.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), cancellable = true, method = "onGameMessage")
-	public void onGameMessage(GameMessageS2CPacket packet, CallbackInfo ci){
-		onMessage.done(packet, ci);
+	@Shadow
+	@Nullable
+	public HitResult crosshairTarget;
+
+	@Shadow
+	@Nullable
+	public ClientWorld world;
+
+	@Inject(at = @At("HEAD"), cancellable = true, method = "doAttack")
+	public void onAttack(CallbackInfo ci) {
+		if (this.crosshairTarget.getType() == HitResult.Type.BLOCK) {
+			BlockHitResult blockHitResult = (BlockHitResult) this.crosshairTarget;
+			if (world.getBlockState(blockHitResult.getBlockPos()).getBlock() instanceof CropBlock){
+				ci.cancel();
+				FarmUtil.areaReplace();
+			}
+		}
 	}
 }
